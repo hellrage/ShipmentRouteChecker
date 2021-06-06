935d9c930fdf015773d1336a629ae6df83951c6f
Hellrage <hellrage.k@gmail.com>
2021-06-06 13:01:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeliveryRouteChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
